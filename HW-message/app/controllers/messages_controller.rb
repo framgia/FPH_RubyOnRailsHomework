25855260ec6347212ff089675f0e4020cf3eb6ac
sanoyo <sa00380304yo@gmail.com>
2017-07-17 19:00:39 +0800

    Create Error Message

--- HW-message/app/controllers/messages_controller.rb
+++ HW-message/app/controllers/messages_controller.rb
@@ -7,8 +7,15 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path, notice: 'メッセージを保存しました'
+    if @message.save
+        flash[:success] = 'メッセージを保存しました'
+        # redirect_to root_path, notice: 'メッセージを保存しました'
+        redirect_to root_path
+    else
+        @messages = Message.all
+        flash.now[:alert] = 'メッセージの保存に失敗しました'
+        render 'index'
+    end
   end
 
   private
--- HW-message/app/models/message.rb
+++ HW-message/app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ApplicationRecord
+    validates :name, length: { maximum: 30 }, presence: true
+    validates :body, length: { minimum: 2, maximum: 255 }, presence: true
 end
