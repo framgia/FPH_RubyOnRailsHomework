75bacefb8ed0feea5b570907f436f76d54e083d1
sanoyo <sa00380304yo@gmail.com>
2017-07-20 10:21:38 +0800

    Set Index

--- suggestionbox/config/routes.rb
+++ suggestionbox/config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   get 'topics/new'
 
   get 'topics/edit'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
